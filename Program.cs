0e176825eca13920b0385254815401f1cb1b3a3e
RUTHIE <ruthifeobasi1@gmail.com>
2021-07-31 16:01:06 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace sayHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Hello Ruth!!!");
+         Console.WriteLine("oh hi, julzz.... how are you doing today?");
+         Console.WriteLine("i am doing quite alright thanks");
+         Console.WriteLine("good to hear daer :)...");
+          
+
+    
+        }
+    }
+}
